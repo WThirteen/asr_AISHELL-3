3d87cd1c9a18472d502722fd9db8919b9ec0d28d
WThirteen <Thirteen.Shisan@outlook.com>
2024-05-14 00:21:21 +0800

    训练、识别、配置文件
    train 为训练文件
    asr 为识别文件
    config 为配置文件

---
+++ config.py
@@ -0,0 +1,29 @@
+# 训练数据音频文件路径
+train_wav_data_path = 'AISHELL-3/train/wav/'
+
+# 训练数据内容文件路径
+train_texts_data_path = 'AISHELL-3/train/content.txt'
+
+# 测试数据音频文件路径
+test_wav_data_path = 'AISHELL-3/test/wav/'
+
+# 测试数据内容文件路径
+test_texts_data_path = 'AISHELL-3/test/content.txt'
+
+# 存放模型路径 /模型名字
+model_path = 'asr_AISHELL.h5'
+
+# 存放pkl路径 /pkl名字
+pkl_path = 'dictionary.pkl'
+
+# model_name
+
+batch_size = 16
+
+epochs = 50
+
+num_blocks = 3
+
+filters = 128
+
+mfcc_dim = 13
